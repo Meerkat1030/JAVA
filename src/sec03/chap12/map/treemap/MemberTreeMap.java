572df29e4b7a91ca3f71b6f52b280678812c5919
meerkat1030 <hong991000@gmail.com>
2023-11-16 15:13:31 +0900

    [JAVA] treemap 연습

---
+++ src/sec03/chap12/map/treemap/MemberTreeMap.java
@@ -0,0 +1,39 @@
+package sec03.chap12.map.treemap;
+
+import sec03.chap12.collection.Member;
+
+import java.util.Iterator;
+import java.util.TreeMap;
+
+public class MemberTreeMap {
+    private TreeMap<Integer, Member> treeMap;
+
+    public MemberTreeMap() {
+        treeMap = new TreeMap<Integer, Member>();
+    }
+
+    //회원 추가
+    public void addMember(Member member){
+        treeMap.put(member.getMemberId(), member);
+
+    }
+    //회원 삭제
+    public boolean removeMember(int memberId){
+        if(treeMap.containsKey(memberId)){
+            treeMap.remove(memberId);
+            return true;
+        }
+        System.out.println(memberId+"가 존재하지 않음");
+        return false;
+    }
+    //회원 전체 보기
+    public void showAllMember(){
+        Iterator<Integer> ir = treeMap.keySet().iterator();
+        while (ir.hasNext()){
+            int key = ir.next();
+            Member member = treeMap.get(key);
+            System.out.println(member);
+        }
+        System.out.println();
+    }
+}
